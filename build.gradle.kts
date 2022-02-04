57d7d427af96315936035f781e362f07b52c026d
Jochen Guck <Jochen.Guck@snk.de>
2022-02-04 07:35:50 +0100

    feat: Enable matrix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 if (System.getenv("GITHUB_RUN_NUMBER") != null) {
     version = "1.0.${System.getenv("GITHUB_RUN_NUMBER")}"
-}else{
+} else {
     version = "1.0.0"
 }
 
@@ -31,7 +31,7 @@ subprojects {
             isReproducibleFileOrder = true
         }
     }
-    if(doNotPublish.contains(this.name)){
+    if (doNotPublish.contains(this.name)) {
         return@subprojects
     }
     apply(plugin = "maven-publish")
@@ -41,8 +41,10 @@ subprojects {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/dcxp/opentelemetry-kotlin")
                 credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                    username =
+                        project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                    password =
+                        project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
                 }
             }
         }
@@ -54,3 +56,60 @@ rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJ
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>()
         .nodeVersion = "16.13.1"
 }
+// Build pipeline Tasks
+tasks.register("checkMac") {
+    dependsOnTaskOfSubprojectsByName("macosArm64Test")
+    dependsOnTaskOfSubprojectsByName("macosX64Test")
+    dependsOnTaskOfSubprojectsByName("IosArm64Test")
+    dependsOnTaskOfSubprojectsByName("IosArm32Test")
+}
+
+tasks.register("checkWindows") {
+    dependsOnTaskOfSubprojectsByName(":mingwX64Test")
+}
+
+tasks.register("checkLinux") {
+    dependsOnTaskOfSubprojectsByName("jvmTest")
+    dependsOnTaskOfSubprojectsByName("JsTest")
+    dependsOnTaskOfSubprojectsByName("linuxX64Test")
+    dependsOnTaskOfSubprojectsByName("linuxArm32HfpTest")
+}
+
+tasks.register("publishMac") {
+    dependsOnTaskOfSubprojectsByName("publishIosArm32PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishIosArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishIosSimulatorArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishIosSimulatorArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishTvosArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishTvosSimulatorArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishTvosX64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishWatchosArm32PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishWatchosArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishWatchosSimulatorArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishWatchosX86PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishMacosArm64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishMacosX64PublicationToMavenRepository")
+}
+
+tasks.register("publishWindows") { dependsOnTaskOfSubprojectsByName("publishMingwX64PublicationToMavenRepository") }
+
+tasks.register("publishLinux") {
+    dependsOnTaskOfSubprojectsByName("publishLinuxX64PublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishLinuxArm32HfpPublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishJvmPublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishJsPublicationToMavenRepository")
+    dependsOnTaskOfSubprojectsByName("publishKotlinMultiplatformPublicationToMavenRepository")
+}
+
+fun getTaskOfSubprojectsByName(name: String): List<Task> {
+    return project.subprojects.mapNotNull { project -> project.tasks.findByName(name) }
+}
+
+fun Task.dependsOnTaskOfSubprojectsByName(name: String){
+    this.dependsOn(getTaskOfSubprojectsByName(name))
+}
+/*fun dependsOnTaskOfSubprojectsByName(name: String){
+    getTaskOfSubprojectsByName(name).forEach{task ->
+        task()
+    }
+}*/
